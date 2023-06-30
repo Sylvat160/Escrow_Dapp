e2ae6dd2154beeb86bfd1ba815cd27fa0eb3d8c7
Sylvain T <sylvaintagnabou@gmail.com>
2023-06-30 16:51:30 +0000

    (add) asset

--- front/src/assets/index.js
+++ front/src/assets/index.js
@@ -3,11 +3,13 @@ import setting from './setting.svg'
 import alertIcon from './alertIcon.svg';
 import AlertIcon from './AlertIcon.jsx';
 import copy from './copy.svg';
+import ok from './ok.svg';
 
 export {
     logo,
     setting,
     alertIcon,
     AlertIcon,
-    copy
+    copy,
+    ok
 }
